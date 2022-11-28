14e08eab45d13acd8a0d268b3a3ccfaaf0c827c8
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-11-28 14:58:38 +0530

    Removing the top element in the satck using Peek and Pop method

--- StackAndQueueProblems/StackAndQueueProblems/Program.cs
+++ StackAndQueueProblems/StackAndQueueProblems/Program.cs
@@ -1,11 +1,18 @@
 ﻿
 
 using StackAndQueueProblems;
-
+//UC1
 StackPushOperation obj = new StackPushOperation();
 obj.push(70);
 obj.push(30);
 obj.push(56);
 
 obj.display();
+
+//UC2
+StackPOpAndPeakOperation obj2 = new StackPOpAndPeakOperation();
+obj2.push(70);
+obj2.push(30);
+obj2.push(56);
+obj2.TillEmpty();
 
---
+++ StackAndQueueProblems/StackAndQueueProblems/StackPOpAndPeakOperation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblems
+{
+    public class StackPOpAndPeakOperation
+    {
+         Node top;
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+        }
+
+        public void display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("stack is empty");
+            }
+            while (top != null)
+            {
+                Console.Write("|" + top.data + "|");
+                top = top.next;
+            }
+        }
+
+        public void Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Console.WriteLine("{0} removed from the satck",top.data);
+            top = top.next;
+        }
+
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is empty");
+            }
+            Console.WriteLine("{0} is on the top of the Satck",this.top.data);
+        }
+        
+        public void TillEmpty()
+        {
+           
+            while (this.top != null)
+            {             
+                Peek();
+                Console.WriteLine();
+                Pop();
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
