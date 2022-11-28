1e3a04b0828315c4ceb16c11af8146cfa5c6fb32
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-11-28 13:47:22 +0530

    Ability to create a Stack of 56->30->70

---
+++ StackAndQueueProblems/StackAndQueueProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using StackAndQueueProblems;
+
+StackPushOperation obj = new StackPushOperation();
+obj.push(70);
+obj.push(30);
+obj.push(56);
+
+obj.display();
+
---
+++ StackAndQueueProblems/StackAndQueueProblems/StackPushOperation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblems
+{
+    public class StackPushOperation
+    {
+        public Node top;
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+               node.next = top;
+            }
+            top = node;
+        }
+
+        public void display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("stack is empty");
+            }
+           while(top != null)
+            {
+                Console.Write("|"+top.data+"|");
+                top = top.next;
+            }
+        }
+    }
+    
+}
